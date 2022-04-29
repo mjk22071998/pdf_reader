1d1984f15966e6728035122b91091a8e720098f1
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-04-30 04:12:37 +0500

    theme updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PDF reader',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: const ColorScheme.light(
+          background: Colors.white,
+          onBackground: Colors.black,
+          error: Colors.red,
+          onError: Colors.white,
+          primary: Colors.teal,
+          onPrimary: Colors.white,
+          primaryContainer: Color.fromARGB(255, 0, 63, 57),
+          secondary: Colors.tealAccent,
+          onSecondary: Colors.black,
+          secondaryContainer: Color.fromARGB(255, 0, 63, 57),
+          surface: Colors.white,
+          onSurface: Colors.black,
+          onPrimaryContainer: Colors.white,
+          onSecondaryContainer: Colors.white,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'PDF reader'),
     );
   }
 }
